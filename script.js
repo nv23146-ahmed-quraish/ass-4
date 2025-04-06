c36926ea5ae5b661861229a55df89e61d572e1ce
nv23146-ahmed-quraish <Nv23146@nvtc.edu.bh>
2025-04-06 16:49:43 +0300

    Step-3

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+function getComputerChoice() {
+  const randomNum = Math.random();
+  if (randomNum < 1/3) {
+    return "rock";
+  } else if (randomNum < 2/3) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log("Computer Choice:", getComputerChoice());
